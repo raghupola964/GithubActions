1d52c6f3c8b8ed5407587bf9333bb4fc1dc28ae3
raghupola964 <126030021+raghupola964@users.noreply.github.com>
2024-04-16 15:58:41 -0400

    Update calci.py

--- src/calci.py
+++ src/calci.py
@@ -4,3 +4,10 @@ def add(a,b):
 def test_add():
   assert add(1, 3) == 4
   assert add(1, -4) == -3
+
+def sub(a,b):
+  return a-b
+
+def test_sub():
+  assert sub(1, 3) == -2
+  assert sub(1, -4) == 5
